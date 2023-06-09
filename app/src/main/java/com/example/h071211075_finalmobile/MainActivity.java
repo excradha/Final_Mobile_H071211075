6bc7aa7c577cbe80782d2e2be93a955161e326c5
excradha <realradha76@gmail.com>
2023-06-09 21:15:07 +0800

    first commit

---
+++ app/src/main/java/com/example/h071211075_finalmobile/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.h071211075_finalmobile;
+
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/h071211075_finalmobile/TvShowFragment.java
@@ -0,0 +1,4 @@
+package com.example.h071211075_finalmobile;
+
+public class TvShowFragment {
+}
